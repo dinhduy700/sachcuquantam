0a1bc12ef96e4ecb3e91f588927484edde2be926
EC2 Default User <ec2-user@ip-172-31-28-206.ap-southeast-1.compute.internal>
2025-06-18 14:49:00 +0000

    add route learning-cicd

---
+++ resources/views/learning-ci-cd.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta charset="utf-8">
+</head>
+<body>
+  <label for="">Name</label>
+	<input type="text" name="name" />
+	<script src="http://127.0.0.1:8000/assets/libs/jquery/jquery.min.js"></script>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -7,5 +7,9 @@ Route::get('/test', function(){
 Route::get('/test2', function(){
 	return view('test2');
 });
+
+Route::get('learning-ci-cd', function(){
+	return view('learning-ci-cd');
+});
 include 'be.php';
 include 'fe.php';
